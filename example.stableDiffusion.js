0b9792aad4237dba8ae4b80c20105e65703c3967
MatthieuWevox <mgeiss@wevox.eu>
2023-10-25 23:01:42 +0200

    [UPD] Portrait mode for the picture

--- example.stableDiffusion.js
+++ example.stableDiffusion.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 let stableDiffusionUrl = "https://clipdrop-api.co/text-to-image/v1";
 
 //let prompt = "A cat in a hat in Pixar style";
-let prompt = "Create a heartwarming Pixar-style image capturing Ron Weasley's essence from Harry Potter. Illustrate his warmth, courage, and loyalty, showcasing his quirky humor and fierce protectiveness. Craft Ron in Gryffindor robes, surrounded by his iconic exclamations like 'Blimey!' and 'Bloody hell!'. Highlight his endearing clumsiness and unwavering friendship with Harry and Hermione. Let his genuine, caring nature shine through in a scene that captures the magic of camaraderie and bravery.";
+let prompt = "Create a heartwarming Pixar-style portrait capturing Ron Weasley's essence from Harry Potter. Illustrate his warmth, courage, and loyalty, showcasing his quirky humor and fierce protectiveness. Craft Ron in Gryffindor robes, surrounded by his iconic exclamations like 'Blimey!' and 'Bloody hell!'. Highlight his endearing clumsiness and unwavering friendship with Harry and Hermione. Let his genuine, caring nature shine through in a scene that captures the magic of camaraderie and bravery.";
 
 let formData = new FormData();
 formData.append("prompt", prompt);
